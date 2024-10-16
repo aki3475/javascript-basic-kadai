13c69d2ee66c82babe684a22ad6f351c7b6f50c0
aki3475 <aki10313475@gmail.com>
2024-10-16 20:22:03 +0900

    再提出

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,2 +1,5 @@
 const Btn = document.getElementById('btn');
-Btn.addEventListener('click',()=>{setTimeout(()=>{document.getElementById('text').innerHTML='<h2>ボタンをクリックしました<h2>';},2000);});
+const Text=document.getElementById('text');
+Btn.addEventListener('click',()=>{setTimeout(()=>{Text.innerHTML='ボタンをクリックしました';},2000);});
+
+
